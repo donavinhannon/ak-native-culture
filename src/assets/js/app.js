00659e2e3cfe109cd19912d9276a975864840a4a
Donavin <donavinhannon@gmail.com>
2017-07-25 20:43:56 -0700

    Updated img links in activities.json, updated json call js code

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,9 +8,23 @@ function getJson(url, callback) {
     xhr.send(null);
 }
 
-function getTabContent(tab) {
-    getJson('https://www.calaska.org/assets/json/activities.json', function() {
-        var tabContentHtml = document.getElementById('tab-content');
-        tabContentHtml.innerHTML = '<div class="container grid"><div class="grid__col is-12"><p class="activity__text">' + tab.text + '</p></div><div class="grid__col text-center is-12--small is-4--medium is-4--large"><img class="media" src="' + tab.desktopImg1 + '" alt="' + tab + ' Picture 1"></div><div class="grid__col text-center is-12--small is-4--medium is-4--large"><img class="media" src="' + tab.desktopImg2 + '" alt="' + tab + ' Picture 2"></div><div class="grid__col text-center is-12--small is-4--medium is-4--large"><img class="media" src="' + tab.desktopImg3 + '" alt="' + tab + ' Picture 3"></div></div>';
+function getTabContent(tabName) {
+    getJson('https://www.calaska.org/assets/json/activities.json', function(o) {
+        parseTabContent(o, tabName);
     });
+}
+
+function parseTabContent(o, tabName) {
+    const json = JSON.parse(o);
+    let results = json.Drums;
+    if (tabName == 'Paddles') {results = json.Paddles;}
+    else if (tabName == 'Beads') {results = json.Beads;}
+    else if (tabName == 'Weaving') {results = json.Weaving;}
+    else if (tabName == 'Bags') {results = json.Bags;}
+    else {results = json.Drums;}
+
+
+    const tabContentHtml = document.getElementById('tab-content');
+    tabContentHtml.innerHTML = '<div class="container grid"><div class="grid__col is-12"><p class="activity__text">' + results.text + '</p></div><div class="grid__col text-center is-12--small is-4--medium is-4--large"><img class="media" src="' + results.desktopImg1 + '" alt="' + tabName + ' Picture 1"></div><div class="grid__col text-center is-12--small is-4--medium is-4--large"><img class="media" src="' + results.desktopImg2 + '" alt="' + tabName + ' Picture 2"></div><div class="grid__col text-center is-12--small is-4--medium is-4--large"><img class="media" src="' + results.desktopImg3 + '" alt="' + tabName + ' Picture 3"></div></div>';
+
 }
