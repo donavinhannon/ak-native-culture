4b60f5bd334b08434698a91be1a69132c7def0d0
Donavin <donavinhannon@gmail.com>
2017-07-25 17:12:43 -0700

    Added activities json file json call function, and event listeners to tab buttons

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,9 @@
-  console.log('The dog hoped over the fence.');
-  console.log('the fence.');
-  console.log('Yellow Jello Mellow.');
-  document.getElementById('about').innerHTML = 'Marry Poppins is Dope';
+function getJson(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4 && xhr.status == 200)
+            callback(xhr.responseText);
+    };
+    xhr.open('GET', url, true); // true for asynchronous
+    xhr.send(null);
+}
