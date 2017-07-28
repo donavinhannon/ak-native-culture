5cb1bb303d275bd19c5dcbc5e2270f27602c7b3f
Donavin <donavinhannon@gmail.com>
2017-07-28 13:02:54 -0700

    First cc2015 commit, TODO: choose responsive image slider or gallery for event pics

--- gulpfile.js
+++ gulpfile.js
@@ -16,18 +16,27 @@ var sourcemaps    = require('gulp-sourcemaps');
 var babel         = require('gulp-babel');
 
 // JS Task
-gulp.task('js-compile', function () {
-  return gulp.src('src/assets/js/**/*.js')
+// gulp.task('js-bundle', function () {
+//   return gulp.src('src/assets/js/**/*.js')
+//     .pipe(plumber())
+//     .pipe(babel())
+//     .pipe(gulp.dest('dist/assets/js'))
+// });
+
+gulp.task('js-minify', function () {
+  return gulp.src('src/assets/js/app.js')
     .pipe(plumber())
-    .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(concat('bundle.js'))
+    .pipe(sourcemaps.init())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/assets/js'))
+    .pipe(reload({stream:true}));
 });
 
-gulp.task('js-minify', ['js-compile'], function () {
-  return gulp.src('dist/assets/js/bundle.js')
+gulp.task('js-min-slider', function () {
+  return gulp.src('src/assets/js/js-image-slider.js')
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(rename({ suffix: '.min' }))
@@ -37,17 +46,21 @@ gulp.task('js-minify', ['js-compile'], function () {
     .pipe(reload({stream:true}));
 });
 
-gulp.task('js', ['js-compile', 'js-minify']);
+// gulp.task('js', ['js-minify']);
 
+// Css Task
+gulp.task('css', function () {
+  return gulp.src('src/assets/css/*.css')
+    .pipe(plumber())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(reload({stream:true}));
+});
 
 // Sass Task
-// var sassPaths = [
-//   'node_modules/normalize.scss/sass',
-//   'node_modules/foundation-sites/scss',
-//   'node_modules/motion-ui/src'
-// ];
-// Add to sass({includePaths: sassPaths})
-
 gulp.task('sass', function () {
   return gulp.src('src/assets/scss/*.scss')
     .pipe(plumber())
@@ -88,13 +101,15 @@ gulp.task('browser-sync', function() {
 // Watch Task
 gulp.task('live',  ['browser-sync'], function() {
     // Watch scss, js, img, html files
-    gulp.watch('src/assets/scss/**/*', ['sass']);
-    gulp.watch('src/assets/js/**/*.js', ['js']);
+    gulp.watch('src/assets/scss/**/*.scss', ['sass']);
+    gulp.watch('src/assets/css/**/*.css', ['css']);
+    gulp.watch('src/assets/js/**/app.js', ['js-minify']);
+    gulp.watch('src/assets/js/**/js-image-slider.js', ['js-min-slider']);
     gulp.watch('src/assets/img/**/*', ['img']);
     gulp.watch('src/**/*.html', ['html']);
 });
 
 // Default Task
 gulp.task('default', function() {
-    gulp.start('sass', 'js', 'img', 'html');
+    gulp.start('sass', 'css', 'js-minify', 'js-min-slider', 'img', 'html');
 });
